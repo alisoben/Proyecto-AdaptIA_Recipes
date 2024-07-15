394065404d5669586bf786f0499648d935cde2e5
alisoben <abgaaries@gmail.com>
2024-07-15 01:44:49 -0500

    apikey

--- core/config.py
+++ core/config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    API_KEY = os.getenv('GEMINI_API_KEY PROCESANDOSEEEEEE...')
+    API_KEY = os.getenv('AIzaSyCXuGoSxdR4njzo3JMX4yPaDGv0e_6oIbc')
     BASE_URL = 'https://api.gemini.com/v1/' 
 
 config = Config()
